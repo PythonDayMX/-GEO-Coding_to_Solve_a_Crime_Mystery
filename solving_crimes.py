5740370ad1ffde61856471894fbf8363f6c7a090
dulceambrocio <dulce.ambrocio@hotmail.com>
2017-06-09 22:23:41 -0500

    plots and reprojection added

--- solving_crimes.py
+++ solving_crimes.py
@@ -5,6 +5,7 @@ from shapely.geometry import Point
 import pylab as pl
 
 print("hi five!")
+import code; code.interact(local=locals())
 #-----------------------------
 # loading data as dataframes
 #-----------------------------
@@ -33,6 +34,11 @@ print("dataset dtypes: ",dtypes)
 #unique elements
 print("suspects: ",suspects_df['name'].unique())
 
+#len of dataset
+print("len: ",suspects_df.count)
+
+#n rows, n columns
+print("len: ",suspects_df.shape)
 print("have fun: done!")
 
 #-----------------------------
@@ -67,16 +73,18 @@ suspects_gdf['name']=	suspects_gdf['name'].str.title()#st.lower()#str.upper()
 martha 				=	suspects_gdf[suspects_gdf['name']=='Martha']
 
 #check
-print("suspects: ",suspects_df['name'].unique())
+print("suspects: ",suspects_gdf['name'].unique())
 #-----------------------------
 # visualizing
 #-----------------------------
 
+#simple plot
+suspects_gdf.plot(marker='*',color='green')
+pl.show()
 
 #visualizing using CartoDB
 suspects_gdf.to_file('results/suspects_footprints/suspects_footprints.shp')
 victim_gdf.to_file('results/victims_footprints/victims_footprints.shp')
-
 
 
 #-----------------------------
@@ -88,7 +96,16 @@ polygons 			=		geopandas.GeoDataFrame.from_file('settlements/settlements_CDMX.sh
 points_in_polys 	= 		sjoin(suspects_gdf, polygons, how='left')
 
 #new geodataframe inspection
+#this gdf is the result of 
+#match between polygons and points
+#play wit it!
 points_in_polys.columns
 
-import code; code.interact(local=locals())
+#reprojecting
+other_crs = {'init': 'epsg:3395'}
+points_in_polys = points_in_polys.to_crs(other_crs)
 
+#other option
+points_in_polys = points_in_polys.to_crs(polygons.crs)
+
+import code; code.interact(local=locals())
